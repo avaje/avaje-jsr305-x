d5180389ccb154ff46aade5dfd76a0d1f9d0563f
Rob Bygrave <robin.bygrave@gmail.com>
2021-10-20 09:39:26 +1300

    Add meta package with TypeQualifierDefault (to support @NonNullApi)

--- src/main/java/javax/annotation/Nonnull.java
+++ src/main/java/javax/annotation/Nonnull.java
@@ -1,16 +1,12 @@
 package javax.annotation;
 
+import javax.annotation.meta.When;
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
-/**
- * Class retained Nonnull annotation.
- *
- * Purely used to assist IDE auto-completion for users of Ebean API.
- * This is expected to be in maven provided scope only.
- */
 @Documented
 @Retention(RetentionPolicy.CLASS)
 public @interface Nonnull {
+    When when() default When.ALWAYS;
 }
